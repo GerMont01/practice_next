72ea27b9b0f4b56eaa98fba34da84d28bd85021c
GerMont01 <gerardomontme@gmail.com>
2024-11-25 20:30:52 -0800

    products page styling

--- src/app/products/[id]/page.js
+++ src/app/products/[id]/page.js
@@ -2,11 +2,13 @@
 import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 import CartCounter from "@/components/cart_counter";
+import { ChevronDownIcon } from '@heroicons/react/24/outline'
 
 export default function Product() {
     const pathname = usePathname()
 
     const [ device, setDevice ] = useState(undefined)
+    const [ showDetails, setShowDetails ] = useState(false)
     
     const fetchData = async (id) => {
         const response = await fetch(`/api/product_description?id=${id}`);
@@ -19,34 +21,41 @@ export default function Product() {
     },[pathname])
 
     return (
-        <div className="container h-screen max-w-full bg-gray-100 product_description">
+        <div className="container max-w-full bg-gray-100 product_description">
             {device ? (
-                <div className="flex p-10 h-4/5 justify-evenly">     
-                    <div className="h-full">
-                        <h1 className="mb-4 text-2xl font-bold">{device.name}</h1>
-                        <img src={device.picture} alt={device.name} className="mb-4" />
-                        <CartCounter device={device}/>
-                        <p><strong>Brand:</strong> {device.brand_name}</p>
-                        <p><strong>Released At:</strong> {device.released_at}</p>
-                        <p><strong>Body:</strong> {device.body}</p>
-                        <p><strong>OS:</strong> {device.os}</p>
-                        <p><strong>Storage:</strong> {device.storage}</p>
-                        <p><strong>Display Size:</strong> {device.display_size}</p>
-                        <p><strong>Display Resolution:</strong> {device.display_resolution}</p>
-                        <p><strong>Camera Pixels:</strong> {device.camera_pixels}</p>
-                        <p><strong>Video Pixels:</strong> {device.video_pixels}</p>
-                        <p><strong>RAM:</strong> {device.ram}</p>
-                        <p><strong>Chipset:</strong> {device.chipset}</p>
-                        <p><strong>Battery Size:</strong> {device.battery_size}</p>
-                        <p><strong>Battery Type:</strong> {device.battery_type}</p>
+                <div className="flex h-full p-10 justify-evenly">     
+                    <div className="flex flex-col items-center justify-center w-1/2 h-full">
+                        <div className="w-3/5 h-full">
+                            <h1 className="mt-10 mb-4 text-2xl font-bold">{device.name}</h1>
+                            {/* <img src={device.picture} alt={device.name} className="mb-4" /> */}
+                            <p><strong>Brand:</strong> {device.brand_name}</p>
+                            <p><strong>Released At:</strong> {device.released_at}</p>
+                            <p><strong>Body:</strong> {device.body}</p>
+                            <p><strong>OS:</strong> {device.os}</p>
+                            <p><strong>Storage:</strong> {device.storage}</p>
+                            <p><strong>Display Size:</strong> {device.display_size}</p>
+                            <p><strong>Display Resolution:</strong> {device.display_resolution}</p>
+                            <p><strong>Camera Pixels:</strong> {device.camera_pixels}</p>
+                            <p><strong>Video Pixels:</strong> {device.video_pixels}</p>
+                            <p><strong>RAM:</strong> {device.ram}</p>
+                            <p><strong>Chipset:</strong> {device.chipset}</p>
+                            <p><strong>Battery Size:</strong> {device.battery_size}</p>
+                            <p><strong>Battery Type:</strong> {device.battery_type}</p>
+                            <div className="flex items-center mt-6 mb-3 cursor-pointer" onClick={()=>setShowDetails(prev=>!prev)}>
+                                <h2 className="text-xl font-semibold">More details</h2>
+                                <ChevronDownIcon className="size-4"/>
+                            </div>
+                            <ul className="overflow-auto list-disc h-36">
+                                {showDetails &&
+                                    Object.entries(JSON.parse(device.specifications)).map(([key, value]) => (
+                                        <li key={key}><strong>{key.replace(/_/g, ' ')}:</strong> {value}</li>
+                                ))}
+                            </ul>
+                        </div>
                     </div>
-                    <div className="h-full">
-                        <h2 className="mt-16 mb-12 text-xl font-semibold">Specifications:</h2>
-                        <ul className="ml-6 overflow-auto list-disc h-5/6">
-                            {Object.entries(JSON.parse(device.specifications)).map(([key, value]) => (
-                            <li key={key}><strong>{key.replace(/_/g, ' ')}:</strong> {value}</li>
-                            ))}
-                        </ul>
+                    <div className="flex flex-col items-center w-1/2 m-8 bg-white">
+                        <img className="w-2/5 mb-10 m-14" src={device.picture} alt={device.name}/>
+                        <CartCounter  className="w-2/5" device={device}/>
                     </div>
                 </div>
             ) : (
--- src/app/products/page.js
+++ src/app/products/page.js
@@ -53,7 +53,7 @@ export default function Products() {
 
     
     return (
-        <div className="container max-w-full bg-gray-100">
+        <div className="container h-full max-w-full bg-gray-100">
             {/* <Nav /> */}
             <div className="flex w-full place-content-between">
                 <Filter allDevices={allDevices} filters={filterData}/>
@@ -63,14 +63,18 @@ export default function Products() {
                     <>
                         {products.map((product,i)=>(
                             <div className="product" key={i}>
+                                <div className="flex flex-col h-12 m-6 align-middle justify-evenly">
+                                    <p className="text-sm font-bold text-center">{product.name}</p>
+                                    <p className="text-sm font-bold text-center">({product.released_at})</p>
+                                </div>
                                 <img 
                                     src={product.picture} 
                                     className="w-11/12" 
                                     alt={product.name} 
                                     onClick={()=>router.push(`/products/${product.id}`)}
                                 />
-                                <p className="h-16 m-2 text-sm text-center">{product.name} ({product.released_at})</p>
-                                <p className="h-5 m-2 text-sm text-center">$ {product.price}</p>
+                                
+                                <p className="mt-8 mb-2 text-sm font-bold text-center">$ {product.price}</p>
                             </div>
                         ))}
                         <Pagination totalPages={Math.ceil(productQty/20)} />
